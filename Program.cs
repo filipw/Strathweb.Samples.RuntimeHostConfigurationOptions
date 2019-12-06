3c752544cc9e9d30c497d79e0e6417f7abee7b86
filipw <wojcieszyn@gmail.com>
2019-12-06 16:18:30 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config1 = AppContext.GetData("abc");
+            var config2 = AppContext.GetData("foo");
+
+            // prints 123
+            Console.WriteLine(config1);
+
+            //prints bar
+            Console.WriteLine(config2); 
+
+            AppDomain.CurrentDomain.SetData("secret", "hooray");
+            var secret = AppContext.GetData("secret");
+            var secret2 = AppDomain.CurrentDomain.GetData("secret");
+
+            //prints hooray
+            Console.WriteLine(secret);
+
+            // also prints hooray
+            Console.WriteLine(secret2);
+
+            typeof(AppContext).GetMethod("SetData", BindingFlags.Public | BindingFlags.Static).Invoke(null, new object[] { "hello", "world" });
+            var hello = AppContext.GetData("hello");
+
+            //prints bar
+            Console.WriteLine(hello);
+        }
+    }
+}
